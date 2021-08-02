e737006e6f208ea7564dce9723b443f639d23a35
brent <b.hizzes@gmail.com>
2021-08-02 23:53:32 +0100

    asdf

--- analysis/collate_info.py
+++ analysis/collate_info.py
@@ -5,11 +5,13 @@ import csv
 import sys
 import os
 from filenames import filenames
+import datetime
 
 data_dir = sys.argv[1]
 out_fn = sys.argv[2]
 
 data = []
+done = []
 for pid in os.listdir(data_dir):
   # check if finished
   progress_fn = os.path.join(data_dir, pid, filenames['progress'])
@@ -20,8 +22,9 @@ for pid in os.listdir(data_dir):
     for line in f:
       if line.strip().startswith('end\tcompleted'):
         finished = True
+        done.append(pid)
   if not finished:
-    print('unfinished: ', pid)
+    print('unfinished: ', pid, line.strip(), datetime.datetime.fromtimestamp(int(line.strip().split('\t')[-1])/1000))
     continue
   
   # load info
@@ -32,6 +35,9 @@ for pid in os.listdir(data_dir):
       tokens = row.strip().split('\t')
       pid_data[tokens[0]] = tokens[1]
   data.append(pid_data)
+
+for pid in done:
+  print('brent\t' + pid)
 
 if len(data) > 0:
   with open(out_fn, 'w') as f:
